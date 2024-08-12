3236434574c3c6af1e239bd66b89475b7ad39e02
bmphu04 <117680870+bmphu04@users.noreply.github.com>
2024-08-12 16:49:59 +0700

    update

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Funnycode_DoDinhPhu");
+        System.out.println("Hoc CSD tai FPT nhu qq");
     }
 }
